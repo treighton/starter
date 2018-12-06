67b3a7b90085b568d0630a5fcb3c72b3be8a2a5f
Treighton <treighton@pagedesigngroup.com>
2018-12-05 21:36:15 -0800

    testing husky

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 var test = 'poop'
 var poop = false
 
-var ar = [1, 1,1 ]
+var ar = [1, test, poop]
 
-   ar.forEach(item => console.log(item))
+ar.forEach(item => console.log(item))
 
- const add = (a, b) => {
-  return a + b}
+const add = (a, b) => {
+  return a + b
+}
 
-modle.exports = add
+module.exports = add
